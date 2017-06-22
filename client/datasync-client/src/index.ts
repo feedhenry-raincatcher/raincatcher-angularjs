63aa14c2c91bed15d83a77b966f6f05d9bd99bc7
Wojciech Trocki <wtrocki@redhat.com>
2017-06-22 10:46:51 +0100

    Remove unused packages

--- client/datasync-client/src/index.ts
+++
@@ -1,6 +0,0 @@
-import * as sync from 'fh-sync-js';
-
-// Support named import
-export { sync };
-// Support named import
-export default sync;
--- cloud/logger/src/Logger.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Logger {
-    debug(format: any, ...params: any[]): void;
-    error(format: any, ...params: any[]): void;
-    info(format: any, ...params: any[]): void;
-    warn(format: any, ...params: any[]): void;
-}
-
-export default Logger;
