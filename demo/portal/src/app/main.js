e2694c7d5ebf941bcfda15c521abc0670577c407
Wojciech Trocki <wtrocki@redhat.com>
2017-06-26 17:26:53 +0100

    Enable modules in mobile apps

--- demo/portal/src/app/main.js
+++ demo/portal/src/app/main.js
@@ -8,16 +8,16 @@ angular.module('app', [
   require('ng-sortable'),
   require('./feedhenry')
   // Commented until this modules will be migrated
-  // , require('@raincatcher/workorder-angular')({
-  //   mode: "admin",
-  //   listColumnViewId: "column2",
-  //   mainColumnViewId: "content@app"
-  // })
-  // , require('@raincatcher/workflow-angular')({
-  //   mode: "admin",
-  //   listColumnViewId: "column2",
-  //   mainColumnViewId: "content@app"
-  // })
+  , require('@raincatcher/workorder-angular')({
+    mode: "admin",
+    listColumnViewId: "column2",
+    mainColumnViewId: "content@app"
+  })
+  , require('@raincatcher/workflow-angular')({
+    mode: "admin",
+    listColumnViewId: "column2",
+    mainColumnViewId: "content@app"
+  })
 ]);
 
 require('./config');
