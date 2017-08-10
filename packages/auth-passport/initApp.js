12d4038705ad80aa2531c5f2ef95cef1b70daba7
Jameel Briones <jbriones@redhat.com>
2017-08-10 11:49:54 +0100

    Add comment

--- packages/auth-passport/initApp.js
+++ packages/auth-passport/initApp.js
@@ -1,3 +1,7 @@
+/**
+ * Starts the specfied angular app once document is ready
+ * @param appName - The name of the app to be started
+ */
 module.exports = function (appName) {
   angular.element(document).ready(function() {
     angular.bootstrap(document, [appName]);
