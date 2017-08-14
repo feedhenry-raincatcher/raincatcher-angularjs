e5d264d970fb6df33590495207cd3b03eed32de9
Jameel Briones <jbriones@redhat.com>
2017-08-14 16:28:40 +0100

    remove getClientId logic

--- packages/sync/syncGlobalNetworkHandler.js
+++ packages/sync/syncGlobalNetworkHandler.js
@@ -1,15 +1,10 @@
 var cloudURL;
 var cloudPath;
-var cidProvider = require('./clientIdProvider');
 
 module.exports = function createHandler(cloudURL, cloudPath, $http) {
   var handler = function(params, success, failure) {
     var url = cloudURL + cloudPath + params.dataset_id;
     var payload = params.req;
-    payload.__fh = {
-      cuid: cidProvider.getClientId()
-    };
-
     $http.post(url, payload, {}).then(success).catch(failure);
   };
   return handler;
