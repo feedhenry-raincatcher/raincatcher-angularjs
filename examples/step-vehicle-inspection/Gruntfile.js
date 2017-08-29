b8e24e6c40fbbb652061062395812197f1b902ef
Wojciech Trocki <wtrocki@redhat.com>
2017-08-29 18:01:07 +0100

    Support template generation

--- examples/step-vehicle-inspection/Gruntfile.js
+++ examples/step-vehicle-inspection/Gruntfile.js
@@ -3,8 +3,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     eslint: {
       src: ["lib/**/*.js"]
+    },
+    wfmTemplate: {
+      module: "wfm.vehicle-inspection",
+      templateDir: "lib/angular/template",
+      outputDir: "dist"
     }
   });
+
+  grunt.loadNpmTasks('fh-wfm-template-build');
   grunt.loadNpmTasks("grunt-eslint");
   grunt.registerTask('default', ['eslint']);
 };
