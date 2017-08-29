73666c69294213eca06899c2c48dc23bf094ee4d
Wojciech Trocki <wtrocki@redhat.com>
2017-08-29 18:15:53 +0100

    Improve styling

--- examples/step-accident/dist/accident-form.tpl.html.js
+++ examples/step-accident/dist/accident-form.tpl.html.js
@@ -7,35 +7,30 @@ try {
 
 ngModule.run(['$templateCache', function ($templateCache) {
   $templateCache.put('wfm-template/accident-form.tpl.html',
-    '<div layout="row">\n' +
-    '  <div flex="40" layout="row" layout-align="start center">\n' +
-    '    <md-input-container class="md-block">\n' +
-    '      <label>Car registration number</label>\n' +
-    '      <input type="text" id="title" name="title" ng-model="ctrl.model.regNr" required>\n' +
-    '    </md-input-container>\n' +
-    '  </div>\n' +
+    '<p class="wfm-accident-row">Please register driver details</p>\n' +
+    '\n' +
+    '<div class="wfm-accident-row">\n' +
+    '  <md-input-container class="md-block" flex-gt-sm>\n' +
+    '    <label>Car registration number</label>\n' +
+    '    <input type="text" id="title" name="title" ng-model="ctrl.model.regNr" required>\n' +
+    '  </md-input-container>\n' +
+    '  <md-input-container class="md-block" flex-gt-sm>\n' +
+    '    <label>Owner name</label>\n' +
+    '    <input type="text" id="title" name="title" ng-model="ctrl.model.owner" required>\n' +
+    '  </md-input-container>\n' +
+    '  <md-input-container class="md-block" flex-gt-sm>\n' +
+    '    <label>Owner phone number</label>\n' +
+    '    <input type="text" id="title" name="title" ng-model="ctrl.model.phone" required>\n' +
+    '  </md-input-container>\n' +
     '</div>\n' +
     '\n' +
     '<md-divider></md-divider>\n' +
     '\n' +
-    '<div layout="row">\n' +
-    '  <div flex="40" layout="row" layout-align="start center">\n' +
-    '    <md-input-container class="md-block">\n' +
-    '      <label>Owner name</label>\n' +
-    '      <input type="text" id="title" name="title" ng-model="ctrl.model.owner" required>\n' +
-    '    </md-input-container>\n' +
-    '  </div>\n' +
-    '</div>\n' +
     '\n' +
-    '<md-divider></md-divider>\n' +
-    '\n' +
-    '<div layout="row">\n' +
-    '  <div flex="40" layout="row" layout-align="start center">\n' +
-    '    <md-input-container class="md-block">\n' +
-    '      <label>Owner phone number</label>\n' +
-    '      <input type="text" id="title" name="title" ng-model="ctrl.model.phone" required>\n' +
-    '    </md-input-container>\n' +
-    '  </div>\n' +
+    '<div class="workflow-actions md-padding md-whiteframe-z4">\n' +
+    '  <md-button class="md-primary md-hue-1" ng-click="ctrl.back($event)">Back</md-button>\n' +
+    '  <md-button class="md-primary" ng-click="ctrl.done($event)">Continue</md-button>\n' +
     '</div>\n' +
+    '<!-- workflow-actions-->\n' +
     '');
 }]);
