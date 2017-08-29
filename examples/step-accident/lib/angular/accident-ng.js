89c5944080897222b0555f30a82275520c54e7d7
Wojciech Trocki <wtrocki@redhat.com>
2017-08-29 18:27:41 +0100

    Fix build

--- examples/step-accident/lib/angular/accident-ng.js
+++ examples/step-accident/lib/angular/accident-ng.js
@@ -7,35 +7,35 @@ require('../../dist');
 ngModule.directive('accidentReport', function($templateCache) {
   return {
     restrict: 'E'
-  , template: $templateCache.get('wfm-template/accident.tpl.html')
-  , scope: {
-    accidentModel: '=value'
-  }
+    , template: $templateCache.get('wfm-template/accident.tpl.html')
+    , scope: {
+      accidentModel: '=value'
+    }
   };
 });
 
 ngModule.directive('accidentReportForm', function($templateCache) {
   return {
     restrict: 'E'
-  , template: $templateCache.get('wfm-template/accident-form.tpl.html')
-  , scope: {
-  }
-  , controller: function($scope, $state, $stateParams) {
-    var self = this;
-    self.model = {};
-    self.parentController = $scope.$parent;
-    self.back = function(event) {
-      this.parentController.ctrl.triggerBackStep(this.model)
-      event.preventDefault();
-      event.stopPropagation();
-    };
-    self.done = function(event) {
-      this.parentController.ctrl.triggerCompleteStep(this.model)
-      event.preventDefault();
-      event.stopPropagation();
-    };
-  }
-  , controllerAs: 'ctrl'
+    , template: $templateCache.get('wfm-template/accident-form.tpl.html')
+    , scope: {
+    }
+    , controller: function($scope, $state, $stateParams) {
+      var self = this;
+      self.model = {};
+      self.parentController = $scope.$parent;
+      self.back = function(event) {
+        this.parentController.ctrl.triggerBackStep(this.model);
+        event.preventDefault();
+        event.stopPropagation();
+      };
+      self.done = function(event) {
+        this.parentController.ctrl.triggerCompleteStep(this.model);
+        event.preventDefault();
+        event.stopPropagation();
+      };
+    }
+    , controllerAs: 'ctrl'
   };
 });
 
