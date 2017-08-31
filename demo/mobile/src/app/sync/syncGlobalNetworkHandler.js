37bc244976171cbfb072d52a1f1783e27114d60f
Wojciech Trocki <wtrocki@redhat.com>
2017-08-31 15:01:36 +0100

    Remove optional parameter

--- demo/mobile/src/app/sync/syncGlobalNetworkHandler.js
+++ demo/mobile/src/app/sync/syncGlobalNetworkHandler.js
@@ -2,7 +2,7 @@ module.exports = function createHandler(cloudURL, cloudPath, $http) {
   var handler = function(params, success, failure) {
     var url = cloudURL + cloudPath + params.dataset_id;
     var payload = params.req;
-    $http.post(url, payload, {}).then(function(response) {
+    $http.post(url, payload).then(function(response) {
       success(response.data);
     }).catch(function(err) {
       failure(new Error(err));
