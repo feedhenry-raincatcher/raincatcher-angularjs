7eae5474ac696ddf636be2cb6ea941f526f446d4
Paolo Bueno <paolohaji@gmail.com>
2017-09-05 16:22:15 -0300

    Add missing aria label

--- packages/workflow-angular/dist/workflow-process-begin.tpl.html.js
+++ packages/workflow-angular/dist/workflow-process-begin.tpl.html.js
@@ -12,7 +12,7 @@ ngModule.run(['$templateCache', function ($templateCache) {
     '\n' +
     '  <workorder workorder="ctrl.workorder" status="ctrl.workorder.status"></workorder>\n' +
     '  <div ng-if="!ctrl.completed" class="workflow-actions md-padding md-whiteframe-z4">\n' +
-    '    <md-button ng-click="ctrl.begin()" class="md-primary">\n' +
+    '    <md-button aria-label="Begin Workflow" ng-click="ctrl.begin()" class="md-primary">\n' +
     '      <span ng-if="!ctrl.started">Start Workflow</span>\n' +
     '      <span ng-if="ctrl.started">Continue Workflow</span>\n' +
     '    </md-button>\n' +
