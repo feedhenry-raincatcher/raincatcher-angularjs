16425f0c0ab7e5ee1f91eaa8f61e79a8de68baba
Jameel Briones <jbriones@redhat.com>
2017-09-06 12:57:32 +0100

    integrate userService with mobile login controller

--- packages/angularjs-auth/lib/login/login-controller.js
+++ packages/angularjs-auth/lib/login/login-controller.js
@@ -1,45 +1,27 @@
 var CONSTANTS = require('../constants');
 
-/**
- *
- * Controller for user login / logout
- *
- * @param $timeout
- *
- */
-function LoginCtrl($timeout, $http, $state, authService) {
+function LoginCtrl($timeout, $http, $state, $scope, userService, userConfig) {
   var self = this;
+  self.title = userConfig.title || "RainCatcher Mobile";
   self.loginErrorMessage = "";
   self.loginMessages = { success: false, error: false };
   $timeout(function() {
-    self.hasSession = false;
+    if (localStorage.getItem(CONSTANTS.TOKEN_CACHE_KEY)) {
+      self.hasProfileData = true;
+    } else {
+      self.hasProfileData = false;
+    }
   });
   self.login = function(valid) {
     if (valid) {
       self.loginErrorMessage = "";
       self.loginMessages.error = false;
-
-      $http.post('http://localhost:8001/token', {
-        username: self.username,
-        password: self.password
-      }).then(function(res) {
-        localStorage.setItem(CONSTANTS.TOKEN_CACHE_KEY, res.data.token);
-        localStorage.setItem('rcuser_profile', JSON.stringify(res.data.profile));
-        $http({
-          method: 'POST',
-          url: 'http://localhost:8001/login-mobile',
-          headers: {
-            'Authorization': 'JWT ' + localStorage.getItem(CONSTANTS.TOKEN_CACHE_KEY)
-          },
-          data: {}
-        }).then(function() {
-          console.log('success');
-          // TODO: redirect to home page here.
-        }).catch(function(error) {
-          // TODO: Need to handle when server is offline (status: -1)
-          console.log('ERROR: ', error);
+      userService.authenticate(self.username, self.password).then(function(err) {
+        $timeout(function() {
+          self.loginMessages.success = true;
+          $state.go('app.workorder', undefined, {reload: true});
         });
-      }).catch(function(err) {
+      }).catch(function(err){
         $timeout(function() {
           self.loginMessages.error = true;
           self.loginErrorMessage = err;
@@ -49,8 +31,8 @@ function LoginCtrl($timeout, $http, $state, authService) {
   };
 
   self.logout = function() {
-    localStorage.clear();
+    userService.logout();
   };
 }
 
-angular.module(CONSTANTS.AUTH_DIRECTIVE_MODULE).controller('LoginCtrl', ['$timeout', '$http', '$state', 'authService', LoginCtrl]);
+angular.module(CONSTANTS.AUTH_DIRECTIVE_MODULE).controller('LoginCtrl', ['$timeout', '$http', '$state', '$scope', 'userService', 'USER_CONFIG', LoginCtrl]);
