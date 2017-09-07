26b8ac57f6fb4a18107850b3d0f23c2174dd8bca
Jameel Briones <jbriones@redhat.com>
2017-09-07 16:46:38 +0100

    change default route

--- demo/mobile/src/app/initialisation/config.js
+++ demo/mobile/src/app/initialisation/config.js
@@ -2,7 +2,7 @@ function createMainAppRoute($stateProvider, $urlRouterProvider) {
   // if none of the states are matched, use this as the fallback
   $urlRouterProvider.otherwise(function($injector) {
     var $state = $injector.get("$state");
-    $state.go("app.login");
+    $state.go("app.workorder");
   });
   $stateProvider
     .state('app', {
