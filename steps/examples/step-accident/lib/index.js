60573f2c4387a7f068ebc855237008948d52c688
Wojciech Trocki <wtrocki@redhat.com>
2017-09-09 20:20:24 +0100

    Extract definitions from example steps

--- steps/examples/step-accident/lib/index.js
+++ steps/examples/step-accident/lib/index.js
@@ -1,12 +1,5 @@
 module.exports = {
   ngModule: require('./angular'),
-  definition: {
-    code: 'accident-report-form',
-    name: "Accident Report Form",
-    description: "Form used to report vehicle accidents",
-    templates: {
-      form: "<accident-report-form></accident-report-form>",
-      view: "<accident-report value='result.submission'></accident-report>"
-    }
-  }
+  // Definition for this step that is being used to create workorders
+  definition: require('./definition.json')
 };
