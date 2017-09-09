ec279e8f1051866a188aa5b99075bfb15c7318bc
Wojciech Trocki <wtrocki@redhat.com>
2017-09-09 21:05:27 +0100

    Add validation to signature step

--- steps/examples/step-accident/dist/accident-form.tpl.html.js
+++ steps/examples/step-accident/dist/accident-form.tpl.html.js
@@ -9,7 +9,7 @@ ngModule.run(['$templateCache', function ($templateCache) {
   $templateCache.put('wfm-template/accident-form.tpl.html',
     '<p class="wfm-accident-row">Please register driver details</p>\n' +
     '\n' +
-    '<div class="wfm-accident-row">\n' +
+    '<div class="wfm-accident-row" class="form-group" ng-form name="accidentForm">\n' +
     '  <md-input-container class="md-block" flex-gt-sm>\n' +
     '    <label>Car registration number</label>\n' +
     '    <input type="text" id="title" name="title" ng-model="ctrl.model.regNr" required>\n' +
@@ -29,7 +29,7 @@ ngModule.run(['$templateCache', function ($templateCache) {
     '\n' +
     '<div class="workflow-actions md-padding md-whiteframe-z4">\n' +
     '  <md-button class="md-primary md-hue-1" ng-click="ctrl.back($event)">Back</md-button>\n' +
-    '  <md-button class="md-primary" ng-click="ctrl.done($event)">Continue</md-button>\n' +
+    '  <md-button class="md-primary" ng-disabled="accidentForm.$invalid || accidentForm.$pristine" ng-click="ctrl.done($event)">Continue</md-button>\n' +
     '</div>\n' +
     '<!-- workflow-actions-->\n' +
     '');
