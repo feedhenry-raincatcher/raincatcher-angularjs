2c6a8c3281dc25784a710961c7744bc78c1c9631
austincunningham <austincunningham@hotmail.com>
2017-09-12 11:19:57 +0100

    removed camera

--- steps/camera/dist/index.js
+++
@@ -1,1 +0,0 @@
-require('./camera.tpl.html.js');
--- steps/camera/lib/angular/camera-ng.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = 'wfm.camera';
-
-angular.module('wfm.camera', [
-  require('./directive')
-, require('./service')
-]);
