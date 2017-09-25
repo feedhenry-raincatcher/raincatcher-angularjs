be827fba4002f07a9e2630f8fede641927a258c3
Jameel Briones <jbriones@redhat.com>
2017-09-25 16:09:16 +0100

    fix keycloak portal

--- demo/portal/src/app/keycloak/index.js
+++ demo/portal/src/app/keycloak/index.js
@@ -1,19 +1,20 @@
 var keycloakConfig = require('../../config.json');
 var keycloakModule = require('@raincatcher/angularjs-auth-keycloak');
 
-var MOBILE_AUTH_MODULE_ID = "wfm.auth.mobile.keycloak";
+var AUTH_MODULE_ID = "wfm.auth.keycloak";
 
-var angularModule = angular.module(MOBILE_AUTH_MODULE_ID, []);
+var angularModule = angular.module(AUTH_MODULE_ID, []);
+
 var keycloakLib;
 if (window.navigator.onLine) {
-  keycloakLib = keycloakModule.init('wfm-mobile', angularModule,
+  keycloakLib = keycloakModule.init('app', angularModule,
     keycloakConfig.keycloak, keycloakConfig.keycloakInit);
   keycloakModule.interceptor(angularModule, keycloakLib);
 }
-angular.module(MOBILE_AUTH_MODULE_ID).factory('authService', function() {
-  var authService = new keycloakModule.KeycloakAuth(keycloakLib);
+angular.module(AUTH_MODULE_ID).factory('authService', ["$mdDialog", function($mdDialog) {
+  var authService = new keycloakModule.KeycloakAuth(keycloakLib, $mdDialog);
   return authService;
-});
+}]);
 
 
-module.exports = MOBILE_AUTH_MODULE_ID;
+module.exports = AUTH_MODULE_ID;
