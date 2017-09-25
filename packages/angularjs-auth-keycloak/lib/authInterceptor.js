03072db1a62c8820b942ffbe6524d33d6fafbb03
Jameel Briones <jbriones@redhat.com>
2017-09-25 16:09:36 +0100

    updateToken fix

--- packages/angularjs-auth-keycloak/lib/authInterceptor.js
+++ packages/angularjs-auth-keycloak/lib/authInterceptor.js
@@ -19,22 +19,20 @@ function AuthInterceptor(keycloakApi) {
  */
 AuthInterceptor.prototype.request = function request(config) {
   var deferred = q.defer();
-  if (self.keycloakApi.token) {
-    self.keycloakApi.updateToken(5).success(function() {
-      config.headers = config.headers || {};
-      config.headers.Authorization = 'Bearer ' + self.keycloakApi.token;
-      deferred.resolve(config);
-    }).error(function() {
-      // Intentionally do not fail on tokens when offline
-      deferred.reject('Failed to refresh token');
-    });
-    return deferred.promise;
-  }
+  self.keycloakApi.updateToken().success(function () {
+    config.headers = config.headers || {};
+    config.headers.Authorization = 'Bearer ' + self.keycloakApi.token;
+    deferred.resolve(config);
+  }).error(function () {
+    // Intentionally do not fail on tokens when offline
+    deferred.resolve(config);
+  });
+  return deferred.promise;
 }
 
-module.exports = function(angularModule, keycloakApi) {
-  angularModule.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.interceptors.push(function() {
+module.exports = function (angularModule, keycloakApi) {
+  angularModule.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push(function () {
       var interceptor = new AuthInterceptor(keycloakApi);
 
       return new AuthInterceptor(keycloakApi);
