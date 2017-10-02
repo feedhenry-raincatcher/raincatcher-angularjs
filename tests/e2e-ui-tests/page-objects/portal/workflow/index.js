602db66606f71f13c93ac7140288d26bba9c2ada
Jan Hellar <jhellar@redhat.com>
2017-10-02 16:37:30 +0200

    Removed E2E tests

--- tests/e2e-ui-tests/page-objects/portal/workflow/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  main: require('./main.po'),
-  new: require('./new.po'),
-  selected: require('./selected.po')
-};
--- tests/e2e-ui-tests/utils/api/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  auth: require('./auth'),
-  cleanup: require('./cleanup'),
-  users: require('./users'),
-  workorders: require('./workorders'),
-  workflows: require('./workflows')
-}
