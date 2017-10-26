e0acd38c767bcad6c4ff5759cb61430eb5498973
Wojciech Trocki <wtrocki@redhat.com>
2017-10-26 14:10:51 +0100

    Minor fixes

--- steps/examples/step-gallery/lib/angular/urlProvider.js
+++ steps/examples/step-gallery/lib/angular/urlProvider.js
@@ -1,5 +1,7 @@
-
-module.exports = function getUrl(FhLibrary) {
+/**
+ * Helper method fetch base url for the server using feedhenry library.
+ */
+module.exports = function getUrl($fh) {
   return new Promise(function(resolve, reject) {
     // Get server url
     $fh.on('fhinit', function(error) {
