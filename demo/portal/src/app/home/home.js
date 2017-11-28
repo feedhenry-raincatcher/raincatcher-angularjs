43558ac5ad9072402a73641a5cb47e500a734d80
Paolo Bueno <paolobueno@users.noreply.github.com>
2017-11-28 10:27:21 -0200

    Update deps (#132)
    * Update packages in root and add update scripts
    
    * Update dependencies on all packages
    
    * Update lerna version
    
    * fix indent
    
    * Revert update of js sdk due to introduced security issue
    
    * Revert cordova android update
    
    * Update update command
    
    * Remove all caret versions

--- demo/portal/src/app/home/home.js
+++ demo/portal/src/app/home/home.js
@@ -1,17 +1,15 @@
 'use strict';
 angular.module('app.home', ['ui.router'])
-
-.config(function($stateProvider) {
-  $stateProvider
-    .state('app.home', {
-      url: '/home',
-      views: {
-        content: {
-          templateUrl: 'app/home/home.tpl.html'
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('app.home', {
+        url: '/home',
+        views: {
+          content: {
+            templateUrl: 'app/home/home.tpl.html'
+          }
         }
-      }
-    });
-})
-;
+      });
+  });
 
 module.exports = 'app.home';
