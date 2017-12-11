9f48a8b21e34608b59efd0277332fdb772fbdd03
Paolo Bueno <paolohaji@gmail.com>
2017-12-11 20:43:15 -0200

    update auth

--- packages/angularjs-auth/Gruntfile.js
+++ packages/angularjs-auth/Gruntfile.js
@@ -2,24 +2,12 @@
 
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
-  var appConfig = require('./lib/constants')
+  var appConfig = require('./lib/constants');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     eslint: {
       src: ["lib/**/*.js"]
-    },
-
-    mochaTest: {
-      test: {
-        src: ['lib/**/*-spec.js'],
-        options: {
-          reporter: 'Spec',
-          logErrors: true,
-          timeout: 10000,
-          run: true
-        }
-      }
     },
     wfmTemplate: {
       module: appConfig.AUTH_DIRECTIVE_MODULE,
@@ -29,10 +17,8 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('fh-wfm-template-build');
-  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks("grunt-eslint");
-  grunt.registerTask('mocha', ['mochaTest']);
-  grunt.registerTask('unit', ['eslint', 'mocha']);
+  grunt.registerTask('unit', ['eslint']);
   grunt.registerTask('default', ['unit']);
 
 };
