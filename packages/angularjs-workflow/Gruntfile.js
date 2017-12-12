0ff24f1068de881a7b6e1e75dda2a8a10cca93e8
Paolo Bueno <paolohaji@gmail.com>
2017-12-12 14:06:38 -0200

    Revise workflow module

--- packages/angularjs-workflow/Gruntfile.js
+++ packages/angularjs-workflow/Gruntfile.js
@@ -6,17 +6,6 @@ module.exports = function(grunt) {
     eslint: {
       src: ["lib/**/*.js"]
     },
-    mochaTest: {
-      test: {
-        src: ['lib/**/*-spec.js'],
-        options: {
-          reporter: 'Spec',
-          logErrors: true,
-          timeout: 1000,
-          run: true
-        }
-      }
-    },
     wfmTemplate: {
       module: "wfm.workflow.directives",
       templateDir: "lib/template",
@@ -24,6 +13,6 @@ module.exports = function(grunt) {
     }
   });
   grunt.loadNpmTasks('fh-wfm-template-build');
-  grunt.registerTask('test', ['eslint', 'mochaTest']);
+  grunt.registerTask('test', ['eslint']);
   grunt.registerTask('default', ['test']);
 };
